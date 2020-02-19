ef362d716e4215035e40be852e0e13fcf8377dcf
Kevin Jansen <kevitius99@gmail.com>
2020-02-20 00:33:42 +0100

    No user scale

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <meta name="description" content="Bekijk hier hoeveel CMGT Coins jij hebt! Heb jij de meeste?">
 
     <title>CMGT Coin Wallet</title>
