c542c872fbcf51c5f8e4dba41571d7e1e9978774
Sow1995 <leondeklerk12@gmail.com>
2020-02-28 21:31:11 +0100

    added refresh timer, added clickable name

--- resources/js/Pages/Leaderboard.vue
+++ resources/js/Pages/Leaderboard.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="text-white p-8">
+		<inertia-link :href="'/'" class="absolute top-0 right-0 p-8 z-50">Home</inertia-link>
         <div class="flex justify-center" style="flex-direction: column;">
             <h1 class="text-5xl">CMGT Coin Leaderboard</h1>
+			<p class="text-sm mt-4">Deze pagina ververst elke 60 seconden.</p>
             <table class="mx-auto">
                 <tr v-for="user in leaderboard">
-                    <td>{{ user.name }}</td>
+                    <td><a :href="'https://coins.cmgt.dev/?name='+ user.name">{{ user.name }}</a></td>
                     <td>{{ user.value }}</td>
                 </tr>
             </table>
         </div>
-        <!--    <p v-for="user in leaderboard">{{user.name}}: {{user.value}}</p>-->
-        <!--    <p v-for="user in leaderboard">{{ user }}</p>-->
     </div>
 </template>
 
@@ -19,7 +19,12 @@
         name: "Leaderboard",
         props: {
             leaderboard: Array | Object,
-        },
+		},
+		methods() {
+			setTimeout(function(){
+				window.location.reload(1);
+			}, 60000);
+		}
     }
 </script>
 
